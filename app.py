ba414e2fa5953dd5e140fa6fe3b0693d09b03065
Thota Adi Narayana <97646075+adinarayana02@users.noreply.github.com>
2024-09-10 12:33:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 import streamlit as st
-from pages.login import login_page, sign_up_page
+from pages.signup import signup_page
+from pages.login import login_page
 from pages.interview_schedule import interview_schedule_page
+from pages.interview import interview_page
 
 def main():
-    st.sidebar.title("Navigation")
+    st.sidebar.title("AI Interview System")
+    page = st.sidebar.selectbox("Choose a page", ["Sign Up", "Log In", "Interview Schedule", "Interview"])
 
-    if 'logged_in' not in st.session_state:
-        st.session_state.logged_in = False
-
-    if st.session_state.logged_in:
+    if page == "Sign Up":
+        signup_page()
+    elif page == "Log In":
+        login_page()
+    elif page == "Interview Schedule":
         interview_schedule_page()
-    else:
-        page = st.sidebar.radio("Go to", ["Log In", "Sign Up"])
-        if page == "Log In":
-            login_page()
-        elif page == "Sign Up":
-            sign_up_page()
+    elif page == "Interview":
+        interview_page()
 
 if __name__ == "__main__":
     main()
