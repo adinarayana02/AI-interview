f200aa9f3d0fe361de271a2ad20aa24a5d1ed7b6
Thota Adi Narayana <97646075+adinarayana02@users.noreply.github.com>
2024-09-10 12:18:35 +0530

    Update interview_schedule.py

--- pages/interview_schedule.py
+++ pages/interview_schedule.py
@@ -18,7 +18,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="password",
+        password="Thot@adi2002",
         database="interview_system"
     )
 
