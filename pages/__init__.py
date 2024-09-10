74a1e9468636d19fdd63823a374a2c7fe411c9db
Thota Adi Narayana <97646075+adinarayana02@users.noreply.github.com>
2024-09-10 12:49:23 +0530

    Update __init__.py

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,7 +1,3 @@
 # __init__.py
 # pages/__init__.py
 
-from .signup import signup_page
-from .login import login_page
-from .interview_schedule import interview_schedule_page
-from .interview import interview_page
