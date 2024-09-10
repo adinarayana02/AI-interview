673e8577bcb4faa90c3cecfc87a2652ade81d8e2
Thota Adi Narayana <97646075+adinarayana02@users.noreply.github.com>
2024-09-10 13:17:54 +0530

    Update signup.py

--- pages/signup.py
+++ pages/signup.py
@@ -7,16 +7,17 @@ import os
 load_dotenv()
 
 def signup_page():
-    st.title("Sign Up")
+    st.title("Recruiter Sign Up")
 
     with st.form(key='signup_form'):
+        company_name = st.text_input("Company Name")
         username = st.text_input("Username")
         password = st.text_input("Password", type="password")
         email = st.text_input("Email")
         submit_button = st.form_submit_button(label='Sign Up')
 
         if submit_button:
-            if username and password and email:
+            if company_name and username and password and email:
                 connection = mysql.connector.connect(
                     host="localhost",
                     user="root",
@@ -25,9 +26,9 @@ def signup_page():
                 )
                 cursor = connection.cursor()
                 cursor.execute('''
-                    INSERT INTO users (username, password, email)
-                    VALUES (%s, %s, %s)
-                ''', (username, password, email))
+                    INSERT INTO recruiter (company_name, username, password, email)
+                    VALUES (%s, %s, %s, %s)
+                ''', (company_name, username, password, email))
                 connection.commit()
                 cursor.close()
                 connection.close()
